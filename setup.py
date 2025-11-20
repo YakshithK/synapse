c7a3c6c5fe4127ef7007f2b7d83c19cc9a03490e
YakshithK <yakshith.kommineni@gmail.com>
2025-11-20 00:06:12 -0500

    Bump version: 0.2.0 -> 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="agent-synapse",
-    version="0.2.0",
+    version="0.2.1",
     author="Yakshith Kommineni",
     author_email="yakshith.kommineni@gmail.com",
     description="Kubernetes-like orchestration system for AI agents",
